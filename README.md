c018dbd5106e3faa41bca1e67cb4f6e6cc0474d7
shAdowplusing <1290232854@qq.com>
2022-08-16 00:04:30 +0800

    添加正则模块，1~7的练习示例

---
+++ README.md
@@ -0,0 +1,7 @@
+# js_coding
+日常js编程题汇总项目工程
+
+## 同步文档
+1. [数据结构.docx](https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2FshAdow-XJY%2Fnoteview%2Fmaster%2Fassets%2Fa%25E4%25B8%25AD%25E7%25AB%25AF%25E7%25AC%2594%25E8%25AF%2595-%25E6%2595%25B0%25E6%258D%25AE%25E7%25BB%2593%25E6%259E%2584.docx&wdOrigin=BROWSELINK)
+2. [算法代码题汇总.docx](https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2FshAdow-XJY%2Fnoteview%2Fmaster%2Fassets%2Fa%25E4%25B8%25AD%25E7%25AB%25AF%25E7%25AC%2594%25E8%25AF%2595-%25E7%25AE%2597%25E6%25B3%2595%25E4%25BB%25A3%25E7%25A0%2581%25E9%25A2%2598%25E6%25B1%2587%25E6%2580%25BB.docx&wdOrigin=BROWSELINK)
+3. 
---
+++ regex/1. 创建.js
@@ -0,0 +1,33 @@
+// https://blog.csdn.net/weixin_44154094/article/details/112685412
+// https://blog.csdn.net/weixin_44799217/article/details/113704941
+
+// 1. 使用 构造函数 创建正则表达式 ——————即使用var regex = new RegExp("正则表达式","匹配模式");
+
+// ①
+// var re = new RegExp("正则表达式","匹配模式");
+// 匹配模式: i :忽略大小写, g :全局匹配模式, m :多行匹配
+// ②
+// test()
+// -使用这个方法可以用来检查一个字符串是否符合正则表达式的规则，如果符合则返回true，否则返回false
+
+
+// 用来检查字符串中是否含有a
+// 大小写忽略
+var regex1 = new RegExp("a","i");
+console.log(regex1.test("ABCDEFG"));
+// 区分大小写
+var regex11 = new RegExp("a","");
+console.log(regex11.test("ABCDEFG"));
+
+
+// 2. 使用字面量创建一个正则表达式  ——————即使用var regex = /正则表达式/匹配模式
+// 用来检查字符串中是否含有abc
+// 大小写忽略
+var regex2 = /abc/i ;
+console.log(regex2.test("ABCDEFG"));
+// 区分大小写
+var regex22 = /abc/ ;
+console.log(regex22.test("ABCDEFG"));
+
+
+
